07f3468441050fad570e3379b1177fafaa66cb9c
AnithaThilagar <anithathilagar@gmail.com>
2018-09-14 10:30:54 +0530

    Initial code commit for Fleetcor

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 config ={
-	"amazonProfileURL"  : "https://api.amazon.com/user/profile?access_token=",
-    'claimStatusApiURL' : 'http://35.154.116.87:8080/cc/service/edge/claim/vhv',
-	'serviceURL'		: 'https://fleetcor-response.herokuapp.com/'
+	"amazonProfileURL"  : "https://api.amazon.com/user/profile?access_token="
 }
 module.exports = config;
--- server.js
+++ server.js
@@ -5,31 +5,29 @@ const express = require('express'),
     alexa = require('alexa-app'),
     app = express(),
     alexaApp = new alexa.app("fleetcorassistant"),
-    helper = require('./helper'),
-	MongoClient = require('mongodb').MongoClient;
+    MongoClient = require('mongodb').MongoClient;
 	
 require('dotenv').config();
-	
-/*mongoose.connect(process.env.MONGODB_URL + process.env.MONGODB_NAME);
-var db = mongoose.connection;
-console.log(db.collections);
-db.on("error",console.error.bind(console,"Connection error"));
-db.once("open",function(callback){
-  console.log('Connection Succeeded');
-});*/
 
-/*MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
-  if (err) { 
-	console.log("Error in getting connection ", err);
-  } else {
-	  console.log("Connection Succeeded");
-  }
-});*/
-
-alexaApp.express({
-    expressApp: app,
-    checkCert: false
+//create server to listen to port from the environment variable or 5000
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
+
+/*MongoClient.connect(process.env.MONGODB_URL + process.env.MONGODB_NAME, function(err, db) {
+	if (err) { 
+		console.log("Error in getting connection ", err);
+	} else {	  
+		db.collection("fleetcor_user_card").find({"lastFour": "1234"}).toArray((error, result) => {
+			if(error){
+				console.log(error);
+			} else {
+				console.log(result);
+			}
+			db.close();
+		});
+	}
+});*/
 
 var blockCardIntentCalled = false;
 var creditLimitIntentCalled = false;
@@ -52,10 +50,7 @@ alexaApp.accountLinkingCard = function () {
 }
 
 alexaApp.launch(async function (request, response) {
-    //console.log('launch ' + JSON.stringify(request));
-    //console.log('Session Obj ' + JSON.stringify(request.getSession()));
-    //console.log('Session Obj is new ' + request.getSession().isNew());
-    //locale = request.data.request.locale;
+    console.log('Session Obj ' + JSON.stringify(request.getSession()));
     var say = [];
 	//Check if the Access Token is there
 	if (request.getSession().details.accessToken) {
@@ -123,180 +118,40 @@ alexaApp.intent('yesIntent', function (request, response) {
  });
  
 alexaApp.intent('cardNumberIntent', function (request, response) {
-    
     var say =['default response'];
     console.log(request.data.request.intent.slots.cardNumber.value)
     cardNumber=request.data.request.intent.slots.cardNumber.value;
-    //if(claimId.length==11){
-        console.log('length 11');
-        /*claimId = (claimId.replace(/(\d{3})(\d{2})(\d{6})/, "$1-$2-$3"));
-        console.log('After change::',claimId);*/
-        if(blockCardIntentCalled){
-            console.log('Inside blockCardIntentCalled');
-            //return helper.getClaimStatus(claimId).then((result)=>{
-                say = ["<s> Are you sure that you want to block the card ending with <say-as interpret-as='ordinal'>"+cardNumber+" </say-as></s>"];
-                console.log('after call',say);
-                response.shouldEndSession(false, "Tell yes to block or no to not block the card");
-                response.say(say.join('\n'));
-
-            // }).catch((err)=>{
-            //     say = ["<s> Something went wrong while processing your request.</s><s>Please try again</s>"];
-            //     response.shouldEndSession(true);
-            //     response.say(say.join('\n'));				
-            // })
-        }
-        else if(creditLimitIntentCalled){
-            console.log("inside api call");
-           // return helper.getClaimPaymentDetails(claimId).then((result)=>{            
-                say = ["<s>You have a credit limit of <break strength=\"medium\" /> $250 in your card </s>"];
-                claimPaymentDetails = result;
-                console.log('after call',say);
-                response.shouldEndSession(false);
-                response.say(say.join('\n'));         
-            // }).catch((err)=>{
-            //     say = ["<s> Something went wrong while processing your request.</s><s>Please try again</s>"];
-            //     response.shouldEndSession(true);
-            //     response.say(say.join('\n'));				
-            // })
-            // console.log(say);
-        }
-        else if(accountBalanceIntentCalled){
-            return helper.getRentalCarStatus(claimId).then((result) => {            
-                say = result;
-                console.log('after call',say);
-                response.shouldEndSession(false);
-                response.say(say.join('\n'));         
-            }).catch((err)=>{
-                say = ["<s> Something went wrong while processing your request.</s><s>Please try again</s>"];
-                response.shouldEndSession(true);
-                response.say(say.join('\n'));				
-            })
-        }
-    /*}
-    else{
-        console.log('length not 11');
-        say=['<s>please enter the complete claim number</s>'];
-        response.shouldEndSession(false);
-        response.say(say.join('\n'));
-    }*/
-    
+    console.log('length 11');
+	if(blockCardIntentCalled){
+		console.log('Inside blockCardIntentCalled');
+		say = ["<s> Are you sure that you want to block the card ending with <say-as interpret-as='ordinal'>"+cardNumber+" </say-as></s>"];
+		console.log('after call',say);
+		response.shouldEndSession(false, "Tell yes to block or no to not block the card");
+		response.say(say.join('\n'));
+	}
+	else if(creditLimitIntentCalled){
+		console.log("inside api call");
+		say = ["<s>You have a credit limit of <break strength=\"medium\" /> $250 in your card </s>"];
+		claimPaymentDetails = result;
+		console.log('after call',say);
+		response.shouldEndSession(false);
+		response.say(say.join('\n'));
+	}
+	else if(accountBalanceIntentCalled){
+		return helper.getRentalCarStatus(claimId).then((result) => {            
+			say = result;
+			console.log('after call',say);
+			response.shouldEndSession(false);
+			response.say(say.join('\n'));         
+		}).catch((err)=>{
+			say = ["<s> Something went wrong while processing your request.</s><s>Please try again</s>"];
+			response.shouldEndSession(true);
+			response.say(say.join('\n'));				
+		})
+	}
 });
 
-/*
-alexaApp.intent('GermanClaimStatusIntent', function (request, response) {
-    var all = JSON.parse(request.session('all') || '{}');
-    claimStatusIntentCalled = true;
-    console.log(request.data.request.intent.slots)
-    var say=[];
-    
-    if (request.data.request.intent.slots.claimId.value){
-        claimId=request.data.request.intent.slots.claimId.value;
-        console.log('claimId:'+claimId);
-        claimIdPresent = true;
-        claimId = (claimId.replace(/(\d{3})(\d{2})(\d{6})/, "$1-$2-$3"));
-        return helper.getClaimStatusGerman(claimId).then((result)=>{
-            say = result;
-            console.log('after call',say);
-            response.shouldEndSession(false);
-            response.say(say.join('\n'));
-
-        }).catch((err)=>{
-            say = ["<s> Bei der Bearbeitung Ihrer Anfrage ist ein Fehler aufgetreten.</s><s>Bitte versuche es erneut</s>"];
-            response.shouldEndSession(true);
-            response.say(say.join('\n'));				
-        })
-    }
-    else{
-     say = ["<s>Bitte geben Sie die Anspruchsnummer an. <break strength=\"medium\" /></s>"];
-    }
-    response.shouldEndSession(false);
-    response.say(say.join('\n'));
-});
-
-
-
-alexaApp.intent('GermanClaimIdIntent', function (request, response) {
-    var all = JSON.parse(request.session('all') || '{}');
-    var say =[];
-    console.log(request.data.request.intent.slots.claimId.value)
-    claimId=request.data.request.intent.slots.claimId.value;
-    claimId = (claimId.replace(/(\d{3})(\d{2})(\d{6})/, "$1-$2-$3"));
-    if(claimStatusIntentCalled){
-        return helper.getClaimStatusGerman(claimId).then((result)=>{
-            say = result;
-            console.log('after call',say);
-            response.shouldEndSession(false);
-            response.say(say.join('\n'));
-
-        }).catch((err)=>{
-            say = ["<s> Bei der Bearbeitung Ihrer Anfrage ist ein Fehler aufgetreten.</s><s>Bitte versuche es erneut</s>"];
-            response.shouldEndSession(true);
-            response.say(say.join('\n'));				
-        })
-    }
-    /*if(repairPaymentIntentCalled){
-        getRepairPaymentStatus(claimId,function(responseText){
-            say = responseText;
-        });
-    }
-    if(rentalCarIntentCalled){
-        getRentalCarStatus(claimId,function(responseText){
-            say = responseText;
-        });
-    }
-    response.shouldEndSession(false);
-    response.say(say.join('\n'));//*
-});
-
-alexaApp.intent('rentalConfirmIntent', function (request, response) {
-    var all = JSON.parse(request.session('all') || '{}');
-    var say = ["<s> As per your policy, you are eligible for 30 days rental car service not exceeding $35 a day.</s>"];
-    say.push('<s> Can you let me know the start date of the rental car service?</s>');
-    response.shouldEndSession(false);
-    response.say(say.join('\n'));
-});
-
-alexaApp.intent('rentalCancelIntent', function (request, response) {
-    var all = JSON.parse(request.session('all') || '{}');
-    var say =["<s> Okay,But you can book a rental car later!</s>"];
-    response.shouldEndSession(true);
-    response.say(say.join('\n'));
-    resetAll();
-});
-
-alexaApp.intent('rentalDetailsIntent', function (request, response) {
-    var all = JSON.parse(request.session('all') || '{}');
-    var say =[];
-    console.log(request.data.request.intent.slots);
-    if (request.data.request.intent.slots.startDate.value && rentalStartDate =='' ){
-        rentalStartDate = request.data.request.intent.slots.startDate.value;
-        console.log(rentalStartDate);
-        say =["<s> Can you tell me for how many days you would require the rental car service?</s>"];
-    }
-    if(rentalStartDate==''){
-        say =["<s> Can you let me know the start date of the rental car service?</s>"];
-    }
-    if(request.data.request.intent.slots.days.value && rentalDays ==''){
-        rentalDays = request.data.request.intent.slots.days.value;
-        return helper.getRentalConfirmation(claimId,rentalStartDate,rentalDays).then((result)=>{            
-            say = result;
-            console.log('after call',say);
-            response.shouldEndSession(false);
-            response.say(say.join('\n'));         
-        }).catch((err)=>{
-            say = ["<s> Something went wrong while processing your request.</s><s>Please try again</s>"];
-            response.shouldEndSession(true);
-            response.say(say.join('\n'));				
-        })
-    }
-    if(rentalDays==''){
-        say =["<s> Can you tell me for how many days you would require the rental car service?</s>"];
-    }
-   // var say =["<s> Happy to help you!</s>"];
-    response.shouldEndSession(false);
-    response.say(say.join('\n'));
-});*/
-
+//To handle if user wants to end the conversation
 alexaApp.intent('thankIntent', function (request, response) {
     var all = JSON.parse(request.session('all') || '{}');
     var say =["<s> Happy to help you!</s> Good bye"];
@@ -305,18 +160,6 @@ alexaApp.intent('thankIntent', function (request, response) {
 });
 
 
-if (process.argv.length > 2) {
-    var arg = process.argv[2];
-    if (arg === '-s' || arg === '--schema') {
-        console.log(alexaApp.schema());
-    }
-    if (arg === '-u' || arg === '--utterances') {
-        console.log(alexaApp.utterances());
-    }
-}
-const server = app.listen(process.env.PORT || 5000, () => {
-    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-});
 
 //To get the profile details of user using the Access Token
 function getUserDetails(accessToken){
